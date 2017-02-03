4710da33d1568d23ff11af839ff2b39d23fa4b9f
zevwl <zevwl@bhphoto.com>
2017-02-03 00:13:51 -0500

    Added frontend validation

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+$('form').validate({
+    rules: {
+        ".alphanumeric": {
+            alphanumeric: true
+        }
+    },
+    errorPlacement: function ( error, element ) {
+        // Add the `help-block` class to the error element
+        error.addClass( "help-block" );
+
+        if ( element.prop( "type" ) === "checkbox" ) {
+            error.insertAfter( element.parent( "label" ) );
+        } else {
+            error.insertAfter( element );
+        }
+    },
+    highlight: function ( element, errorClass, validClass ) {
+        $( element ).parents( ".form-group" ).addClass( "has-error" ).removeClass( "has-success" );
+    },
+    unhighlight: function (element, errorClass, validClass) {
+        $( element ).parents( ".form-group" ).addClass( "has-success" ).removeClass( "has-error" );
+    }
+});
